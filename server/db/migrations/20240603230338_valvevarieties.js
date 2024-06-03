669c8722d24fe1d6bd6f7b0ade750f4bd45994a6
Conor Burke-Govey <21695840+cburkeg@users.noreply.github.com>
2024-06-04 11:11:09 +1200

    Add valvevarieties table and seed data

---
+++ server/db/migrations/20240603230338_valvevarieties.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('valvevarieties', (table) => {
+    table.increments('id')
+    table.string('valve_variety')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('valvevarieties')
+}
---
+++ server/db/seeds/test_varieties.js
@@ -0,0 +1,14 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function seed(knex) {
+  // Deletes ALL existing entries
+  await knex('valvevarieties').del()
+  await knex('valvevarieties').insert([
+    { id: 1, valve_variety: 'Pentode (signal)' },
+    { id: 2, valve_variety: 'Pentode (output)' },
+    { id: 3, valve_variety: 'Triode (signal)' },
+    { id: 4, valve_variety: 'Triode (output)' },
+  ])
+}
